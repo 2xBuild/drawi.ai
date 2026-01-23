c136a416a36dd60237510fd1eb24403b5fcea2ac
2xBuild <ha29rp@gmail.com>
2026-01-23 16:00:21 +0530

    .

--- apps/frontend/src/routes/Draw.tsx
+++ apps/frontend/src/routes/Draw.tsx
@@ -2,10 +2,37 @@ import { Excalidraw } from "@excalidraw/excalidraw"
 import "@excalidraw/excalidraw/index.css";
 
 
+function MyButton() {
+  return <button>My Button</button>
+}
+
+function MySidebar() {
+  return <div>My Sidebar</div>
+}
+
 export default function DrawPage() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Excalidraw />
+    
+<Excalidraw
+  UIOptions={{
+    tools: {
+      selection: true,
+      rectangle: true,
+      diamond: false,
+      ellipse: true,
+      arrow: false,
+      line: true,
+      text: true,
+      image: false,
+      eraser: true,
+      hand: true,
+      frame: false,
+      laser: false,
+    },
+  }}
+/>
+
     </div>
   )
 }
